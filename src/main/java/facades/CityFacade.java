778f3751fa16a7ddfbc3d1093d968c615d79791c
Annika Ehlers <e_hlers@live.dk>
2019-10-11 17:13:22 +0200

    Fixed zipcodes

--- src/main/java/facades/CityFacade.java
+++ src/main/java/facades/CityFacade.java
@@ -5,12 +5,15 @@
  */
 package facades;
 
+import data.Zip;
 import entities.CityInfo;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.NoResultException;
@@ -67,18 +70,16 @@ public class CityFacade {
     
     public void addCities() throws FileNotFoundException, IOException {
         EntityManager em = getEntityManager();
-        System.out.println();
-        
-        BufferedReader reader = new BufferedReader(new FileReader(System.getProperty("user.dir") + "\\zipcodes.txt"));
-        em.getTransaction().begin();
-        String row = "";
-        while((row = reader.readLine()) != null) {
-            String[] data = row.split(" ", 2);
-            em.persist(new CityInfo(Integer.parseInt(data[0]), data[1]));
-            
+        HashMap<Integer, String> zips = Zip.zipcodes;
+        try {
+            em.getTransaction().begin();
+            //zips.forEach((k, v) -> em.persist(new CityInfo(k, v)));
+            for(Map.Entry<Integer, String> kv: zips.entrySet()) {
+                em.persist(new CityInfo(kv.getKey(), kv.getValue()));
+            }
+            em.getTransaction().commit();
+        } finally {
+            em.close();
         }
-        em.getTransaction().commit();
-        em.close();
-        reader.close();
     }
 }
